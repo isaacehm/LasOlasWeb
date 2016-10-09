7a903361d045bbdb111ef3db11ea47c88b6f0b56
Isaac Herrera <isaacehm@gmail.com>
2016-10-09 17:12:49 -0400

    g-f

--- app/app.js
+++ app/app.js
@@ -66,8 +66,8 @@ config(['$routeProvider', function($routeProvider) {
 
 .factory('API', ['$http', function($http) {
 
-    //var socket = io('http://localhost:3000'); FOR DEVELOPMENT ONLY
-    var socket = io('http://oneidea.com.ar:3000'); // FOR PRODUCTION
+    var socket = io('http://localhost:3000'); //FOR DEVELOPMENT ONLY
+    //var socket = io('http://oneidea.com.ar:3000'); // FOR PRODUCTION
 	var employee = null;
 	var users = null;
     var categories = null;
